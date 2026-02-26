cf5b2de31c2098f5ee0bc53cffbf583286eba6fa
Kraxiloth <kraxiloth@proton.me>
2026-02-26 09:34:35 +0100

    Functional prototype.

---
+++ js/setData.js
@@ -0,0 +1,37 @@
+const SET_DATA = {
+    "Alpha": {
+        collection: 403,
+        playset: 1081
+    },
+    "Beta": {
+        collection: 402,
+        playset: 1077
+    },
+    "Alpha-Beta": {
+        collection: 407,
+        playset: 1082
+    },
+    "Arthurian Legends": {
+        collection: 230,
+        playset: 550
+    },
+    "Dragonlord": {
+        collection: 14,
+        playset: 14
+    },
+    "Promotional": {
+        collection: 63,
+        playset: 63
+    },
+    "Gothic": {
+        collection: 447,
+        playset: 1115,
+        avatars: 13,
+        elements: {
+            Air: { Unique: 22, Elite: 23, Exceptional: 24, Ordinary: 29 },
+            Earth: { Unique: 20, Elite: 21, Exceptional: 26, Ordinary: 27 },
+            Fire: { Unique: 23, Elite: 19, Exceptional: 22, Ordinary: 27 },
+            Water: { Unique: 22, Elite: 21, Exceptional: 25, Ordinary: 27 },
+        }
+    }
+}
